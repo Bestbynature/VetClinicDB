e5466218696ca66303f54b5fc49ad99a6556303e
Dammy <bestbynature2010@gmail.com>
2023-06-04 17:18:09 +0100

    :rocket: Add the Database schema to keep the structure of entire database.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(
+    animal_id INT GENERATED ALWAYS AS IDENTITY,
+    id INT, 
+    name varchar(20), 
+    date_of_birth date, 
+    escape_attempts INT, 
+    neutered bit, 
+    weight_kg decimal, 
+    PRIMARY KEY(animal_id)
+    );
