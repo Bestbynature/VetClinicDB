055ca169e9b1fd9960449a68c9172288d43a4f94
Dammy <bestbynature2010@gmail.com>
2023-06-05 11:48:42 +0100

    :rocket: Update data file

--- data.sql
+++ data.sql
@@ -5,3 +5,5 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '2018-11-15', 2, '1', 8.00), 
 (3, 'Pikachu', '2021-01-07', 1, '0', 15.04), 
 (4, 'Devimon', '2017-05-12', 5, '1', 11.00);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', '2020-02-08', 0, '0', 11.00), (6, 'Plantmon', '2021-11-15', 2, '1', 5.70), (7, 'Squirtle', '1993-04-02', 3, '0', 12.13), (8, 'Angemon', '2005-06-12', 1, '1', 45.00), (9, 'Boarmon', '2005-06-07', 7, '1', 20.40), (10, 'Blossom', '1998-10-13', 3, '1', 17.00), (11, 'Ditto', '2022-05-14', 4, '1', 22.00);
