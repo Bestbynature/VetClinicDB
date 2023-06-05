06c97eddad576068705000513c502ebdc04d7792
Dammy <bestbynature2010@gmail.com>
2023-06-05 18:55:47 +0100

    :rocket: Update queries dot sql file

--- queries.sql
+++ queries.sql
@@ -49,3 +49,49 @@ ght FROM animals GROUP BY species;
 
 SELECT species, avg(escape_attempts) as avg_esc_attempts FROM animals w
 here date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+-- foreign key
+
+SELECT a.name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
+WHERE o.full_name = 'Melody Pond';
+
+
+SELECT a.name
+FROM animals a
+JOIN species s ON a.species_id = s.id
+WHERE s.name = 'Pokemon';
+
+SELECT o.full_name, a.name
+FROM owners o
+LEFT JOIN animals a ON o.id = a.owner_id;
+
+
+SELECT s.name, COUNT(*) AS animal_count
+FROM species s
+JOIN animals a ON s.id = a.species_id
+GROUP BY s.name;
+
+
+SELECT a.name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
+JOIN species s ON a.species_id = s.id
+WHERE o.full_name = 'Jennifer Orwell' AND s.name = 'Digimon';
+
+
+SELECT a.name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
+JOIN species s ON a.species_id = s.id
+WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
+
+
+
+SELECT o.full_name, COUNT(*) AS animal_count
+FROM owners o
+JOIN animals a ON o.id = a.owner_id
+GROUP BY o.full_name
+ORDER BY animal_count DESC
+LIMIT 1;
