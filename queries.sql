c11f6679e60c1348cf6b31366934db705f25202e
Dammy <bestbynature2010@gmail.com>
2023-06-05 11:49:26 +0100

    :rocket: Update query file

--- queries.sql
+++ queries.sql
@@ -14,4 +14,38 @@ SELECT * FROM animals WHERE neutered = '1';
 
 SELECT * FROM animals WHERE name <> 'Gabumon';
 
-SELECT * FROM animals WHERE weight_kg between 10.4 and 17.3;
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species='pokemon' WHERE name NOT LIKE '%mon';
+
+DELETE FROM animals;
+
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+
+SAVEPOINT firstPoint;
+
+ UPDATE animals SET weight_kg=weight_kg * -1;
+
+ROLLBACK to SAVEPOINT firstPoint;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg<0;
+
+COMMIT;
+
+ SELECT count(name) as name_of_animal FROM animals;
+
+SELECT count(escape_attempts) FROM animals WHERE escape_attempts=0;
+
+SELECT avg(weight_kg) FROM animals;
+
+ SELECT neutered, count(*) as escape_count FROM animals GROUP BY neutere
+d order by escape_count desc;
+
+ SELECT species, min(weight_kg) as min_weight, max(weight_kg) as max_wei
+ght FROM animals GROUP BY species;
+
+SELECT species, avg(escape_attempts) as avg_esc_attempts FROM animals w
+here date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
